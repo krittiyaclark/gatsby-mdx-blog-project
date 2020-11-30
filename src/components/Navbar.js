4930daa4bd805a8b9c8518735686d52e2ca2dd69
krittiyaclark <krittiyaclark@gmail.com>
2020-11-30 13:28:18 -0500

    Installed @mdx-js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { FaBars } from 'react-icons/fa'
-import logo from '../assets/logo.svg'
-import Links from '../constants/links'
-import SocialLinks from '../constants/socialLinks'
+import React from "react"
+import { Link } from "gatsby"
+import { FaBars } from "react-icons/fa"
+import logo from "../assets/logo.svg"
+import Links from "../constants/links"
+import SocialLinks from "../constants/socialLinks"
+
 const Navbar = ({ toggle }) => {
   return <h2>navbar component</h2>
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import Layout from '../components/Layout'
-import Posts from '../components/Posts'
-import { graphql } from 'gatsby'
-import SEO from '../components/SEO'
+import React from "react"
+import Hero from "../components/Hero"
+import Layout from "../components/Layout"
+import Posts from "../components/Posts"
+import { graphql } from "gatsby"
+import SEO from "../components/SEO"
+
 // ...GatsbyImageSharpFluid
 const IndexPage = () => {
-  return <h2>home page</h2>
+  return <h1>home page</h1>
 }
 
 export default IndexPage
