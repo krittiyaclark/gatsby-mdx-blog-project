037b9c9c636b03cda9abeebe97e39ab45c9e85d5
krittiyaclark <krittiyaclark@gmail.com>
2020-12-01 18:45:45 -0500

    Added the posts component

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,43 @@ import Posts from "../components/Posts"
 import { graphql } from "gatsby"
 import SEO from "../components/SEO"
 
-// ...GatsbyImageSharpFluid
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  // console.log(data)
+  const {
+    allMdx: { nodes: posts },
+  } = data
   return (
     <Layout>
       <Hero showPerson />
+      <Posts posts={posts} title="recent published" />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allMdx(limit: 3, sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        excerpt
+        frontmatter {
+          title
+          author
+          category
+          date(formatString: "MMMM, Do, YYYY")
+          slug
+          readTime
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
 
 export default IndexPage
