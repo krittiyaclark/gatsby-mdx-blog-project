eb945bb8d99a8df21bf6c79df655fe219283c812
krittiyaclark <krittiyaclark@gmail.com>
2020-12-01 13:41:08 -0500

    Embedded Counter and Like Button in MDX & Single Post Setup

--- src/components/Complete/LikeButton.js
+++ src/components/Complete/LikeButton.js
@@ -1,6 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 import { AiOutlineLike } from "react-icons/ai"
+
 const LikeButton = () => {
   const [value, setValue] = React.useState(0)
   return (
